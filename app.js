6d5fc32cdaea2f34caa40c133796d7cdd2d9d94f
Will Gundy <willgunderson503@gmail.com>
2022-03-08 12:26:23 -0800

    Added handle guess function to each event listener

--- app.js
+++ app.js
@@ -1,33 +1,60 @@
 // import functions and grab DOM elements
-const cup0ButtonEl = document.getElementById('btn-cup0');
-const cup1ButtonEl = document.getElementById('btn-cup1');
-const cup2ButtonEl = document.getElementById('btn-cup2');
+const cupButtonEl0 = document.getElementById('btn-cup0');
+const cupButtonEl1 = document.getElementById('btn-cup1');
+const cupButtonEl2 = document.getElementById('btn-cup2');
 
-const cup0ImageEl = document.getElementById('img-cup0');
-const cup1ImageEl = document.getElementById('img-cup1');
-const cup2ImageEl = document.getElementById('img-cup2');
+const cupImageEl0 = document.getElementById('img-cup0');
+const cupImageEl1 = document.getElementById('img-cup1');
+const cupImageEl2 = document.getElementById('img-cup2');
 
 const winTotalEl = document.getElementById('winCount');
 const lossTotalEl = document.getElementById('lossCount');
 const totalCountEl = document.getElementById('totalCount');
-// let state
 
+// let state
 let gamesTotal = 0;
 let winsTotal = 0;
 
 // set event listeners
 
-cup0ButtonEl.addEventListener('click', () => {
-    const numberBetweenZeroAndThree = Math.floor(3 * Math.random());
-    console.log(numberBetweenZeroAndThree);
+cupButtonEl0.addEventListener('click', () => {
+    handleGuess(0, getWinningCup());
 });
-cup1ButtonEl.addEventListener('click', () => {
-    console.log('btn2');
+cupButtonEl1.addEventListener('click', () => {
+    handleGuess(1, getWinningCup());
 });
-cup2ButtonEl.addEventListener('click', () => {
-    console.log('btn3');
+cupButtonEl2.addEventListener('click', () => {
+    handleGuess(2, getWinningCup());
 });
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
 
+function handleGuess(userGuess, winningCup) {
+    //reset cup images
+    resetCupImages()
+    //update game and win counts
+    gamesTotal++;
+    if (userGuess === winningCup) {
+        winsTotal++;
+    }
+
+    //update dom with new count info
+    winTotalEl.textContent = winsTotal;
+    lossTotalEl.textContent = gamesTotal - winsTotal;
+    totalCountEl.textContent = gamesTotal;
+
+    //update image for winning cup
+    const winningCupImage = document.getElementById('img-cup' + winningCup);
+    winningCupImage.src = 'assets/cup-ball.png';
+}
+
+function getWinningCup() {
+    return Math.floor(3 * Math.random());
+}
+
+function resetCupImages() {
+    cupImageEl0.src = 'assets/cup.png';
+    cupImageEl1.src = 'assets/cup.png'; 
+    cupImageEl2.src = 'assets/cup.png'; 
+}
