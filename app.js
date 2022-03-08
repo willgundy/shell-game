dbb53bf513d3a797ba2f764eb19e0582b9ecb5cc
Will Gundy <willgunderson503@gmail.com>
2022-03-08 11:48:27 -0800

    Added html elements for page

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 // import functions and grab DOM elements
+const cup1ButtonEl = document.getElementById('btn-cup1');
+const cup2ButtonEl = document.getElementById('btn-cup2');
+const cup3ButtonEl = document.getElementById('btn-cup3');
 
+const cup1ImageEl = document.getElementById('img-cup1');
+const cup2ImageEl = document.getElementById('img-cup2');
+const cup3ImageEl = document.getElementById('img-cup3');
+
+const winTotalEl = document.getElementById('winCount');
+const lossTotalEl = document.getElementById('lossCount');
+const totalCountEl = document.getElementById('totalCount');
 // let state
+
+let gamesTotal = 0;
+let winsTotal = 0;
 
 // set event listeners 
   // get user input
